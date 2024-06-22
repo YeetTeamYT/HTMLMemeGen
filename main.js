c8da54582949725d34abe935a8368be606d4a139
TheContentMaker3000 <96380370+YeetTeamYT@users.noreply.github.com>
2024-06-22 09:08:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const updateDetails = (url, title, author) => {
 };
 
 const generateMeme = () => {
-  fetch("https://meme-api.com/gimme/cleanmemes")
+  fetch("https://meme-api.com/gimme/wholesomememes")
     .then((response) => response.json())
     .then((data) => {
       updateDetails(data.url, data.title, data.author);
